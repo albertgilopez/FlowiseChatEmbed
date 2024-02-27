4a5f1dd833f066582c78fe89b73aa40428ccb75c
Albert <albert@albertgilopez.es>
2024-02-27 06:44:09 +0100

    badge updated

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -44,7 +44,7 @@ export const Badge = (props: Props) => {
         'background-color': props.badgeBackgroundColor ?? '#ffffff',
       }}
     >
-      Powered by
+      Made with ❤️ by
       <a
         ref={liteBadge}
         href={'https://flowiseai.com'}
@@ -54,7 +54,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> Your Tech Tribe</span>
       </a>
     </span>
   );
